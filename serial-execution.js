0c5941f46f20930d661183af208328744c013150
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-04-01 19:50:31 +0530

    Updated the node.js files

---
+++ serial-execution.js
@@ -0,0 +1,24 @@
+// module fs
+// filesystem
+const fs = require("fs");
+
+console.log("start"); //Non blocking
+fs.readFile("./Nodejs.txt","utf-8", (err, data) => {
+    console.log("We are inside callback function", data); //blocking because it has to wait for the responseof the callback function
+    fs.readFile("./Nodejs.txt","utf-8", (err, data) => {
+        console.log("We are inside callback function", data); //blocking because it has to wait for the responseof the callback function
+        fs.readFile("./Nodejs.txt","utf-8", (err, data) => {
+            console.log("We are inside callback function", data); //blocking because it has to wait for the responseof the callback function
+    
+        //Some activity based on the data of read file
+        });
+    //Some activity based on the data of read file
+    });
+    //Some activity based on the data of read file
+});
+
+console.log("end"); // Non blocking
+
+//start
+//output of filereadning
+//end
